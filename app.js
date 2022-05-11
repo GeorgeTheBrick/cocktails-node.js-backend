1bacc05b294fbfe043f8c342fa3764021dfa1eca
GeorgeTheBrick <jure.brumec@gmail.com>
2022-05-11 09:27:24 +0200

    removed localhost access

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.enable("trust proxy");
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.CORS_ORIGIN, "http://localhost:4200"],
+    origin: [process.env.CORS_ORIGIN],
   })
 );
 
