384f120918d5252dd8f6fe8e4430b140c8b5c538
GeorgeTheBrick <jure.brumec@gmail.com>
2022-05-11 18:50:32 +0200

    trying different headers to enable cors on iphones

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.use(
 
 app.options("/api", cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(helmet());
 
 if (process.env.NODE_ENV === "development") {
