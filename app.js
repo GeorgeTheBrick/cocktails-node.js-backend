6636a1b77f2418ade67858b309361c6e2c982df8
GeorgeTheBrick <jure.brumec@gmail.com>
2022-05-11 13:24:04 +0200

    trying different headers to enable cors on iphones

--- app.js
+++ app.js
@@ -15,15 +15,23 @@ const app = express();
 
 app.enable("trust proxy");
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [process.env.CORS_ORIGIN],
-  })
-);
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
-app.options("/api", cors());
+  next();
+};
 
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: [process.env.CORS_ORIGIN],
+//   })
+// );
+
+// app.options("/api", cors());
+app.use(allowCrossDomain);
 app.use(helmet());
 
 if (process.env.NODE_ENV === "development") {
