58bd997a569cb2242623eec5fd406bbaae8c2f26
GeorgeTheBrick <jure.brumec@gmail.com>
2022-05-12 08:26:12 +0200

    trying different headers to enable cors on iphones

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,6 +28,7 @@ const createSendToken = (user, statusCode, req, res) => {
   res.status(statusCode).json({
     status: "success",
     user,
+    token,
     expiresIn: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ).getTime(),
