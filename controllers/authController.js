ef43b8d281072ba53fdc86876887acdbb968d9e9
Jure <jure.brumec@gmail.com>
2026-05-17 08:43:48 +0200

    revert backend auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,9 @@ const createSendToken = (user, statusCode, req, res) => {
   const cookieDays = parseInt(process.env.JWT_COOKIE_EXPIRES_IN);
 
   res.cookie("jwt", token, {
-    maxAge: cookieDays * 24 * 60 * 60 * 1000,
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
     secure: true,
     sameSite: "None",
@@ -27,7 +29,9 @@ const createSendToken = (user, statusCode, req, res) => {
     status: "success",
     user,
     token,
-    maxAge: cookieDays * 24 * 60 * 60 * 1000,
+    expiresIn: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ).getTime(),
   });
 };
 
@@ -57,7 +61,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie("jwt", "loggedout", {
-    maxAge: 10 * 1000,
+    expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
     secure: true,
     sameSite: "None",
