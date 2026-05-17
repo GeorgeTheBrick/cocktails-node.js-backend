ba38b2e51091e7e3081654fadda7c3182f4e2b7e
Jure <jure.brumec@gmail.com>
2026-05-17 08:11:55 +0200

    resolve logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,7 +57,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie("jwt", "loggedout", {
-    expires: new Date(Date.now() + 10 * 1000),
+    maxAge: 10 * 1000,
     httpOnly: true,
     secure: true,
     sameSite: "None",
