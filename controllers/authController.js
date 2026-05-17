67c16598d4e332a9c7f405c7e1175a234d55132c
Jure <jure.brumec@gmail.com>
2026-05-17 08:52:01 +0200

    revert backend auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,8 +15,9 @@ const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
   const cookieDays = parseInt(process.env.JWT_COOKIE_EXPIRES_IN);
 
+  console.log(process.env.JWT_EXPIRES_IN);
   res.cookie("jwt", token, {
-    expires: new Date(
+    expiresIn: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
@@ -61,7 +62,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie("jwt", "loggedout", {
-    expires: new Date(Date.now() + 10 * 1000),
+    expiresIn: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
     secure: true,
     sameSite: "None",
