0ffe8642a9295eb43ae6cbc85e53445c5bc4dec5
Jure <jure.brumec@gmail.com>
2026-05-17 08:09:55 +0200

    resolve auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,12 +13,10 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
+  const cookieDays = parseInt(process.env.JWT_COOKIE_EXPIRES_IN);
 
   res.cookie("jwt", token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-
+    maxAge: cookieDays * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
     sameSite: "None",
@@ -29,9 +27,7 @@ const createSendToken = (user, statusCode, req, res) => {
     status: "success",
     user,
     token,
-    expiresIn: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ).getTime(),
+    maxAge: cookieDays * 24 * 60 * 60 * 1000,
   });
 };
 
